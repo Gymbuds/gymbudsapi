0ae935f20dff2e778788577a91579c62713c909a
Victor Yuen <92613498+victoryuen@users.noreply.github.com>
2025-03-07 15:11:22 -0500

    fixing format of routers + migration script

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.db.session import check_db_connection  # Import the check_db_connection function
-from app.api.endpoints.init import api_router 
+
+from app.api.endpoints import users, auth
+
 
 # Initialize FastAPI
 app = FastAPI()
-
+app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
 # Check if the database is connected at startup
 @app.on_event("startup")
 async def startup():
@@ -13,4 +16,4 @@ async def startup():
     else:
         print("Failed to connect to the database!")
 
-app.include_router(api_router, prefix="/endpoints", tags=["api"])
+
