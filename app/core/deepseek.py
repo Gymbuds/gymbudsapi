43586bd6f709a4ab5c2a9674a05d15ff2316a769
Victor Yuen <92613498+victoryuen@users.noreply.github.com>
2025-03-20 13:50:42 -0400

    making more changes to get wokrout logsb y user always 30 days if possible

--- app/core/deepseek.py
+++ app/core/deepseek.py
@@ -1,14 +1,18 @@
 from openai import OpenAI
+from sqlalchemy.orm import Session
 import os
 from dotenv import load_dotenv
 from app.schemas.advice import AIAdviceType
 from app.db.crud.workout_log_crud import get_workout_logs_by_user_latest
+from app.db.models.user import User
+from app.db.database import db
+
 load_dotenv()
 
 
-def deepSeekChat(workout_type:str):
+def deepSeekChat(workout_type:str,user:User,db:Session):
     client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url="https://api.deepseek.com")
-    print()
+    user_workouts = get_workout_logs_by_user_latest(db=db,user_id=user.id,latest_amt_days=30)
     # user_preferences = 
     # if workout_type==AIAdviceType.WORKOUT_ADVICE:
     #     messages = [{"role":"system","content":"You are a fitness coach analyzing a user's workout logs and preferences."
