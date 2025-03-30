217cb5d40930bf248f73a9d5ef4c0607efd3652f
Vincent Li <vincentli402@gmail.com>
2025-03-29 20:21:51 -0400

    correct date storage in create health data crud

--- app/db/crud/health_data_crud.py
+++ app/db/crud/health_data_crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import func
 from fastapi import HTTPException
-from datetime import datetime
+from datetime import datetime, timezone
 from app.db.models.health_data import HealthData
 from app.schemas.health_data import HealthDataCreate, HealthDataResponse
 
@@ -13,7 +13,8 @@ def create_health_data(db: Session, user_id: int, health_data: HealthDataCreate)
         calories_burnt=health_data.calories_burnt,
         avg_heart_rate=health_data.avg_heart_rate,
         sleep_duration=health_data.sleep_duration,
-        active_mins=health_data.active_mins
+        active_mins=health_data.active_mins,
+        date=datetime.now(timezone.utc)  # Ensure correct date storage
     )
     db.add(db_health)
     db.commit()
