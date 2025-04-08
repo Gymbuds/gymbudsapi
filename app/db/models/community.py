8578c1fd28b8b04ed1175291d2e85d3d85af6bb3
Victor Yuen <92613498+victoryuen@users.noreply.github.com>
2025-04-08 12:20:20 -0400

    migration script

--- app/db/models/community.py
+++ app/db/models/community.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column,Integer,String,Float
 
 
 class Community(Base):
-    __tablename__ = "availability_ranges"
+    __tablename__ = "gym_communities"
     id = Column(Integer,primary_key=True)
     address = Column(String,primary_key=True)
     latitude = Column(Float,nullable=True)
