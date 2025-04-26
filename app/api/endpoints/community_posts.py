c8c1af1a5e2f010288117dcff77f2b84adbfe691
Vincent Li <vincentli402@gmail.com>
2025-04-25 23:34:21 -0400

    s3 router for com post images

--- app/api/endpoints/community_posts.py
+++ app/api/endpoints/community_posts.py
@@ -1,9 +1,10 @@
-from fastapi import Depends,APIRouter
+from fastapi import Depends,APIRouter, HTTPException
 from app.db.session import get_db
 from sqlalchemy.orm import Session
 from app.schemas.post import CommunityPostCreate,CommunityPostUpdate,CommunityPostResponse,PostCommentCreate,PostCommentResponse
 from app.db.crud.com_post_crud import create_post,update_post,delete_post,get_posts_by_community,add_like_to_post,add_comment_to_post,unlike_a_post,edit_a_comment,delete_a_comment
 from app.core.security import get_current_user 
+from app.core.s3 import create_presigned_upload_url
 from app.db.models.user import User
 from typing import List
 router=APIRouter()
@@ -24,6 +25,26 @@ def update_community_post(
     current_user: User = Depends(get_current_user)
 ):
     return update_post(db, current_user.id, post_id, post_update)
+
+@router.get("/generate-upload-url/")
+def generate_community_post_upload_url(
+    file_extension: str, 
+    db: Session = Depends(get_db), 
+    current_user: User = Depends(get_current_user)
+):
+    if file_extension not in ["jpeg", "jpg", "png"]:
+        raise HTTPException(status_code=400, detail="Invalid file extension")
+
+    presigned_url, s3_file_url = create_presigned_upload_url(
+        user_id=current_user.id,
+        file_extension=file_extension,
+        folder="community_posts"
+    )
+
+    return {
+        "upload_url": presigned_url,  # Used by frontend to PUT the image
+        "file_url": s3_file_url       # Final S3 URL to save into DB
+    }
 
 @router.delete("/{post_id}", status_code=201)
 def delete_community_post(post_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
