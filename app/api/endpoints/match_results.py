9823c94ef031953b2bbc71d155a07269ab0b59ae
Victor Yuen <92613498+victoryuen@users.noreply.github.com>
2025-04-29 14:33:08 -0400

    endpoints for match info

--- app/api/endpoints/match_results.py
+++ app/api/endpoints/match_results.py
@@ -3,6 +3,9 @@ from app.db.session import get_db
 from sqlalchemy.orm import Session
 from app.schemas.match import MatchResultCreate,MatchResultResponse
 from app.db.crud.match_crud import create_match,delete_match,get_matches
+from app.db.crud.user_crud import get_user_info_by_id
+from app.db.crud.range_crud import get_availability_ranges_user
+from app.db.crud.community_crud import get_user_preferred_gym
 from app.core.security import get_current_user
 from app.db.models.user import User
 from app.services.matching import match_users
@@ -25,3 +28,15 @@ def get_user_matches(db:Session=Depends(get_db),current_user:User=Depends(get_cu
 @router.post("/find-match")
 def test_match(db:Session = Depends(get_db),current_user :User = Depends(get_current_user)):
     return match_users(db=db,user= current_user)
+
+@router.get("/user-info/{user_id}")
+def get_user_info_for_match(user_id:int,db:Session = Depends(get_db)):
+    return get_user_info_by_id(db=db,user_id=user_id)
+
+@router.get("/user-range-info/{user_id}")
+def get_user_aval_ranges_for_match(user_id:int,db:Session = Depends(get_db)):
+    return get_availability_ranges_user(db=db,user_id=user_id)
+
+@router.get("/prefer/{user_id}")
+def get_user_pref_for_match(user_id:int,db:Session = Depends(get_db)):
+    return get_user_preferred_gym(db=db,user_id=user_id)
